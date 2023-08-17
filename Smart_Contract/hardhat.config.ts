1f2d506a07c2be932e1d8a3ef9909c119ebc569a
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-08-17 10:45:13 +0300

    Setup deployement of my contract

--- Smart_Contract/hardhat.config.ts
+++ Smart_Contract/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
--- Smart_Contract/scripts/deploy.ts
+++ Smart_Contract/scripts/deploy.ts
@@ -1,4 +1,5 @@
-// import { ethers } from "hardhat";
+
+
 
 // async function main() {
 //   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
@@ -19,9 +20,27 @@
 //   );
 // }
 
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
+// // // We recommend this pattern to be able to use async/await everywhere
+// // // and properly handle errors.
+// // main().catch((error) => {
+// //   console.error(error);
+// //   process.exitCode = 1;
+// // });
+const hre = require("hardhat");
+const { ethers } = hre;
+const main =async ()=>{
+const Transactions = await hre.ethers.getContractFactory("Transactions");
+const transactions = await Transactions.deploy();
+await transactions.deployed();
+console.log("Transaction is deployed to: ",transactions.address);
+}
+const runMain=async()=>{
+    try {
+        await main();
+        process.exit(0)
+    } catch (error) {
+       console.log(error);
+       process.exit(1)
+    }
+}
+runMain()
